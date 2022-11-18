20db58946ad48f1ddfb963687c38b979d42a96d2
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2022-11-18 11:02:39 +0200

    first commit

---
+++ http_preload_scraper.py
@@ -0,0 +1,10 @@
+
+import requests
+import re 
+
+def is_preLoaded(domainName):
+    isLoaded = requests.get(f"https://hstspreload.org/api/v2/status?domain={domainName}").json()
+    return isLoaded["status"] == "preloaded"
+
+print(is_preLoaded("paypal.com"))
+print(is_preLoaded("jct.ac.il"))
