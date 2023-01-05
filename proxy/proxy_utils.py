f27af1885ecbaac1b26b8f1a5e17c7fcab508537
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-01-06 01:55:29 +0200

    create cy-bugs and create dhcp spoofing basic server

--- proxy/proxy_utils.py
+++ proxy/proxy_utils.py
@@ -1,8 +1,29 @@
 
+import gzip
+import os 
 
-def cleanHeaders(): 
-    ... 
-def cleanHostName(): 
-    ... 
-def cleanCookies(): 
-    ... 
+def cleanHeaders(headers : dict  )->dict: 
+    ''' cleans the headers from unwanted headers '''
+    BLACK_LIST = {"content-encoding", "content-length", "transfer-encoding", "connection"} 
+    cleanedHeaders = { }
+    for key in headers : 
+        if key.lower() not in headers: 
+            cleanedHeaders[key] = headers[key]
+    return cleanedHeaders
+
+def cleanHostName(hostName : str )->str: 
+    ''' cleans the spoofed url and returns a valid https orignal hostname '''
+    cleaned = hostName.removeprefix("http://").startswith("vvvvvv.")
+    return f"https://www.{cleaned}/"
+
+def saveImage(imageContent , imageName , path ):
+    os.makedirs(path , exist_ok=True) 
+    with open(f"{path}/images/{path.replace('/','_') if path else 'empty'}" , 'wb') as file : 
+        file.write(respone.content) 
+
+
+def unCompressRespone(respone:bytes) -> bytes :
+    return  gzip.decompress(respone)
+
+def cleanCookies(cookies:str): 
+    ...
--- proxy/reverse_proxy.py
+++ proxy/reverse_proxy.py
@@ -1,25 +1,30 @@
 from flask import Flask, request,make_response
 import requests
-
+import proxy.proxy_utils as proxy_utils
+import proxy.proxy_parser as proxy_parser
 app = Flask(__name__)
 
 
 
-@app.route('/proxy', methods=['GET', 'POST'])
-def proxy():
-    url = request.args.get('url')
-    method = request.method
-    data = request.form if method == 'POST' else request.args
-    respone = requests.request(method, url, data=data)
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def proxy(path):
+    domainName =proxy_utils.cleanHostName(request.host) #get the requested host to reverse proxy 
+    method = request.method #get the method 
+    data = proxy_utils.cleanHeaders(request.form) if method == 'POST' else request.args #get the data section for requests 
+    respone = requests.request(method, domainName+path, data=data)
+    code = respone.status_code
+    headers = proxy_utils.cleanHeaders(respone.headers)
     returnData = None 
-    if "image" in respone.headers['Content-Type'].lower() : 
-         resp =   make_response(respone.content)
+    if "image" in respone.headers['Content-Type'].lower() :  #return an Image 
+         resp = make_response(respone.content)
+         resp.headers.update(headers)
          resp.headers.set('Content-Type',respone.headers ["content-type"] )
-         resp.headers.set('Content-Disposition', 'attachment', filename=sub_path)
+         resp.headers.set('Content-Disposition', 'attachment', filename=path)
+         resp.status_code = code 
          return resp 
     else: 
-         return respone.text
-    
+         returnData = proxy_parser.parse(respone.text)
+    return returnData , code , headers 
 
 if __name__ == '__main__':
     app.run()
